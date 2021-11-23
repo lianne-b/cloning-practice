63ec8b9fb6b83a2fe30f7e25867f140f9a12f185
yen_choi <yenchoi010@gmail.com>
2021-11-23 22:24:31 +0800

    Fix: image position -> fixed to page

--- nomad coder : paint-js/img.js
+++ nomad coder : paint-js/img.js
@@ -1,6 +1,6 @@
 
-const images = ["1.JPG", "2.JPG", "3..JPG", "4.JPG", "5.JPG", "6.JPG", "7.JPG", "8.JPG", "9.JPG", "10.JPG", "11.JPG", "12.JPG", "13.JPG", "14.JPG", "15.JPG"];
-const imgContainer = document.querySelector("#jsImg");
+const images = ["1.JPG", "2.JPG", "3.JPG", "4.JPG", "5.JPG", "6.JPG", "7.JPG", "8.JPG", "9.JPG", "10.JPG", "11.JPG", "12.JPG", "13.JPG", "14.JPG", "15.JPG"];
+const imgContainer = document.querySelector("#jsImgContainer");
 const imgSpan = document.querySelector("#jsSpan");
 
 
@@ -11,7 +11,7 @@ function createImg() {
 }
 
 function removeSpan() {
-    imgSpan.classList.add("remove");
+    imgSpan.classList.add("hidden");
 }
  
 if(imgContainer) {
