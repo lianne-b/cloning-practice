50af7477c7cc308d5604e16441988e348526d978
Yen Choi <yenchoi010@gmail.com>
2022-02-24 23:43:28 +0800

    Feat: add item filtering feature

--- dream-coding_shopping mall game/shopping-game/shopping-template/src/main.js
+++ dream-coding_shopping mall game/shopping-game/shopping-template/src/main.js
@@ -1,17 +1,54 @@
-const colorBlue = document.querySelectorAll(".category.color.blue");
-const colorYellow = document.querySelectorAll(".yellow");
-const colorPink = document.querySelectorAll(".pink");
-const typePants = document.querySelectorAll(".pants");
-const typeSkirt = document.querySelectorAll(".skirt");
-const typeTshirt = document.querySelectorAll(".tshirt");
+"use strict";
 
-const items = document.querySelector(".items");
-const item = document.querySelectorAll(".item");
+const arrayPants = Array.from(document.getElementsByClassName("pants"));
+const arraySkirt = Array.from(document.getElementsByClassName("skirt"));
+const arrayTshirt = Array.from(document.getElementsByClassName("tshirt"));
+const arrayYellow = Array.from(document.getElementsByClassName("yellow"));
+const arrayBlue = Array.from(document.getElementsByClassName("blue"));
+const arrayPink = Array.from(document.getElementsByClassName("pink"));
 
 
-colorBlue.onclick = function filterBlue() {
-    colorYellow.classList.add(".hide");
+function filterBlue() {
+    arrayBlue.map(value => value.classList.remove("visible"));
+    arrayYellow.map(value => value.classList.add("visible"));
+    arrayPink.map(value=> value.classList.add("visible"));
+}
+
+function filterYellow() {
+    arrayYellow.map(value => value.classList.remove("visible"));
+    arrayBlue.map(value => value.classList.add("visible"));
+    arrayPink.map(value=> value.classList.add("visible"));
+}
+
+function filterPink() {
+    arrayPink.map(value => value.classList.remove("visible"));
+    arrayBlue.map(value => value.classList.add("visible"));
+    arrayYellow.map(value => value.classList.add("visible"));
+}
+
+function filterPants() {
+    arrayPants.map(value => value.classList.remove("visible"));
+    arraySkirt.map(value => value.classList.add("visible"));
+    arrayTshirt.map(value=> value.classList.add("visible"));
+}
+
+function filterSkirt() {
+    arraySkirt.map(value => value.classList.remove("visible"));
+    arrayPants.map(value => value.classList.add("visible"));
+    arrayTshirt.map(value=> value.classList.add("visible"));
+}
+
+function filterTshirt() {
+    arrayTshirt.map(value => value.classList.remove("visible"));
+    arrayPants.map(value => value.classList.add("visible"));
+    arraySkirt.map(value=> value.classList.add("visible"));
 }
 
 
-console.log(colorBlue);
+(document.querySelector(".category-c-b")).addEventListener("click", filterBlue);
+(document.querySelector(".category-c-y")).addEventListener("click", filterYellow);
+(document.querySelector(".category-c-p")).addEventListener("click", filterPink);
+
+(document.querySelector(".category-t-p")).addEventListener("click", filterPants);
+(document.querySelector(".category-t-s")).addEventListener("click", filterSkirt);
+(document.querySelector(".category-t-t")).addEventListener("click", filterTshirt);
